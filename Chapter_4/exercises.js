fc330aa39d932f87ecc6831f3b05e482fa7e4a21
Meaghan Alexander <dkothand@gmail.com>
2019-07-01 08:47:57 -0400

    arrayToList function completed

--- Chapter_4/exercises.js
+++ Chapter_4/exercises.js
@@ -80,3 +80,42 @@ const reverseArrayInPlace = (arr) => {
 }
 
 // console.log(reverseArrayInPlace([1, 2, 3, 4, 5]))
+
+
+/***** A List *****/
+/*
+Objects, as generic blobs of values, can be used to build all sorts of data structures.
+A common data structure is the list (not to be confused with array).
+A list is a nested set of objects, with the first object holding a reference to the second, the second to the third, and so on.
+*/
+
+let list = {
+    value: 1, 
+    rest: {
+        value: 2, 
+        rest: {
+            value: 3,
+            rest: null
+        }
+    }
+}
+
+/*
+A nice thing about lists is that they can share parts of their structure. For example, if I create two new values {value: 0, rest: list} and {value: -1, rest: list} (with list referring to the binding de ned earlier), they are both independent lists, but they share the structure that makes up their last three elements. The original list is also still a valid three-element list.
+Write a function arrayToList that builds up a list structure like the one shown when given [1, 2, 3] as argument. Also write a listToArray function that produces an array from a list. Then add a helper function prepend, which takes an element and a list and creates a new list that adds the element to the front of the input list, and nth, which takes a list and a number and returns the element at the given position in the list (with zero referring to the  rst element) or undefined when there is no such element.
+If you haven’t already, also write a recursive version of nth. */
+
+const arrayToList = (arr) => {
+    // Each list element can be decribed as a "node" with two properties
+    // The first is the value of the node
+    // The second is the "pointer" linking the current node to the next node in the list
+    if (arr.length === 0) {
+        return null
+    }
+    const node = {}
+    node["value"] = arr[0]
+    node["rest"] = arrayToList(arr.slice(1))
+    return node
+}
+
+console.log(arrayToList([1, 2, 3]))
