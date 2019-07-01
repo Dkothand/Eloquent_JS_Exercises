8d9933024034d85708764180ba562655980a9c09
Meaghan Alexander <dkothand@gmail.com>
2019-07-01 08:34:13 -0400

    completed Reversing an Array

--- Chapter_4/exercises.js
+++ Chapter_4/exercises.js
@@ -38,8 +38,45 @@ As a bonus assignment, modify your range function to take an optional third argu
 */
 
 const bonusRange = (start, end, step = 1) => {
+    // Only works when start < end and positive step
     const array = []
     for (let i = start; i <= end; i += step) {
-
+        array.push(i)
     }
+    return array
 }
+
+// console.log(bonusRange(1, 10, 2))
+// console.log(bonusRange(5, 2, -1))
+
+
+/***** Reversing an Array *****/
+/*
+Arrays have a reverse method that changes the array by inverting the order in which its elements appear.
+For this exercise, write two functions, reverseArray and reverseArrayInPlace.
+The first, reverseArray, takes an array as argument and produces a new array that has the same elements in the inverse order.
+The second, reverseArrayInPlace, does what the reverse method does: it modifies the array given as argument by reversing its elements. Neither may use the standard reverse method.
+Thinking back to the notes about side e ects and pure functions in the previous chapter, which variant do you expect to be useful in more situations? Which one runs faster?
+*/
+
+const reverseArray = (arr) => {
+    let newArray = []
+    for (let i = arr.length - 1; i >= 0; i--) {
+        newArray.push(arr[i])
+    }
+    return newArray
+}
+// console.log(reverseArray([1, 2, 3, 4, 5]))
+
+
+
+const reverseArrayInPlace = (arr) => {
+    for (let i = 0; i < arr.length / 2; i++) {
+        let temp = arr[i]
+        arr[i] = arr[arr.length - 1 - i]
+        arr[arr.length - 1 - i] = temp
+    }
+    return arr
+}
+
+// console.log(reverseArrayInPlace([1, 2, 3, 4, 5]))
